42065b0d7ad93b155b676609d1311de3eba3957c
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-08-30 17:15:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -20,7 +20,6 @@ def using_delete(instructors, no_offense_steven)
 end
 
 def using_delete_at(famous_robots, deleted_robots)
-  
-  deleted_robots=famous_robots.using_delete_at[2]
+  famous_robots.delete_at(2)
 end
   
